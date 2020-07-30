45588a38800e6a507c4b143ee55ce86f47791f02
Aashish <ashishdahal490@gmail.com>
2020-07-30 15:43:48 +0545

    backend

---
+++ meat-bazar/build/app/generated/source/buildConfig/debug/com/example/masu_bazar/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.masu_bazar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.masu_bazar";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
